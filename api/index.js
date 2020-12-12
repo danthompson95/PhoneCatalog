14442327165867d656a143c19d15a4eb9306e09a
Daniel Thompson <18499520+danthompson95@users.noreply.github.com>
2020-12-12 18:34:46 +0100

    create react app
    Creating the react app for use

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./config/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://danthompsonphonecatalog-default-rtdb.europe-west1.firebasedatabase.app",
+});
+
+const db = admin.database();
+
+app.get("/", (req, res) => {
+  console.log(`Root access`);
+  res.send("API Online");
+});
+
+app.get("/phones", (req, res) => {
+  const phonesRef = db.ref("/phones");
+
+  phonesRef.once(
+    "value",
+    (snapshot) => {
+      console.log("Phone Data Success");
+      res.send(snapshot.val());
+    },
+    (error) => {
+      console.log("Phone Data Failure");
+      res.send(error);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
