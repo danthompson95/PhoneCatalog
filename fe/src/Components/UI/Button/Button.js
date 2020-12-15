a536cae0bf4bffbe192e69ed4d0d893f0f273568
Daniel Thompson <18499520+danthompson95@users.noreply.github.com>
2020-12-15 10:29:03 +0100

    Button Component
    Updated the return function and added styling

--- fe/src/Components/UI/Button/Button.js
+++ fe/src/Components/UI/Button/Button.js
@@ -1,9 +1,9 @@
-const Button = (props) => {
-  return (
-    <button onClick={props.clickEvent} type="button">
-      {props.children}
-    </button>
-  );
-};
+import Styles from "./Button.module.css";
+
+const Button = (props) => (
+  <button className={Styles.Button} onClick={props.clickEvent} type="button">
+    {props.children}
+  </button>
+);
 
 export default Button;
