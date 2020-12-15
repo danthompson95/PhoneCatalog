06e5cb1747c48eba69f589c4e53e82a530cb6be4
Daniel Thompson <18499520+danthompson95@users.noreply.github.com>
2020-12-15 09:17:52 +0100

    Bringing it all together
    Need to add the spinner and finalise any styling

--- fe/src/Components/Phone/Phone.js
+++ fe/src/Components/Phone/Phone.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
+import PhoneImage from "../PhoneImage/PhoneImage";
+import Button from "../UI/Button/Button";
+
+import PhoneDetails from "../PhoneDetails/PhoneDetails";
+
+import Styles from "./Phone.module.css";
+
 const Phone = (props) => {
+  const [phoneDetails, setPhoneDetails] = useState(false);
+
   const phoneData = props.data;
 
+  const handleButtonClick = () => {
+    setPhoneDetails(true);
+  };
+
+  const handleCloseModal = () => {
+    setPhoneDetails(false);
+  };
+
   return (
-    <div>
+    <div className={Styles.Phone}>
       <ul>
+        <PhoneImage
+          image={phoneData.image}
+          alt={`${phoneData.brand} ${phoneData.model}`}
+        />
         <li>{phoneData.brand}</li>
         <li>{phoneData.model}</li>
+        {phoneDetails ? (
+          <PhoneDetails data={phoneData} closeModal={handleCloseModal} />
+        ) : null}
+        <Button clickEvent={handleButtonClick}>View More</Button>
       </ul>
     </div>
   );
