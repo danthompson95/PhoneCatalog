0ed18cac6db19ce6457efbbcd8f8ecd48c7126a6
Daniel Thompson <18499520+danthompson95@users.noreply.github.com>
2020-12-18 08:54:05 +0100

    Delete Phone

--- fe/src/Components/PhoneDetails/PhoneDetails.js
+++ fe/src/Components/PhoneDetails/PhoneDetails.js
@@ -1,5 +1,7 @@
 import Styles from "./PhoneDetails.module.css";
 
+//import Button from "../UI/Button/Button";
+
 const PhoneDetails = (props) => {
   const colors = props.data.colors;
   const colorsList = colors.map((color) => <li key={color}>{color}</li>);
@@ -19,8 +21,45 @@ const PhoneDetails = (props) => {
     <li key={memory}>{memory}</li>
   ));
 
+  /**
+   * I commented out the click handler because it was unnecessary for now
+   */
+
+  // const clickHandler = async () => {
+  //   if (window.confirm("Are you sure you want to delete this?")) {
+  //     if (window.confirm("Are you REALLY sure you want to delete this?")) {
+  //       // The code below works fine, but there is no way to refresh the database upon deletion
+
+  //       /*const data = { id: props.data.id };
+
+  //       const response = await fetch(
+  //         "https://danthompsonphonecatalog.ew.r.appspot.com/deletePhone",
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Access-Control-Allow-Origin": "*",
+  //             "Content-Type": "application/json",
+  //           },
+  //           body: JSON.stringify(data),
+  //         }
+  //       )
+  //         .then((response) => response.text())
+  //         .then((data) => {
+  //           return data;
+  //         })
+  //         .catch((err) => {
+  //           return err;
+  //         });
+
+  //       alert(response);
+  //       window.location.reload();*/
+  //       alert("This would delete the phone but is disabled for the examples");
+  //     }
+  //   }
+  // };
+
   return (
-    <div className={Styles.PhoneDetails} onClick={() => props.closeModal(true)}>
+    <div className={Styles.PhoneDetails}>
       <div className={Styles.PhoneDetailsContent}>
         <div className={Styles.PhoneDetailsHeader}>
           <span
@@ -72,6 +111,9 @@ const PhoneDetails = (props) => {
             <h4 className={Styles.PhoneDetailsCategory}>Colors</h4>
             <ul className={Styles.PhoneDetailsList}>{colorsList}</ul>
           </div>
+        </div>
+        <div className={Styles.PhoneDetailsFooter}>
+          {/*<Button clickEvent={clickHandler}>Delete Phone</Button>*/}
         </div>
       </div>
     </div>
